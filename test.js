a937c8ae93ac782201d8844b0c1048c0af42cd3f
Valerii219 <valera.arl96@gmail.com>
2023-03-18 22:46:49 +0100

    first  attempt

---
+++ test.js
@@ -0,0 +1,8 @@
+function multiply(message, price) {
+    // Тіло функції
+    
+    console.log("Це лог на момент виклику функції multiply");}
+    multiply("JavaScript is in my blood", 10);
+    console.log("JavaScript is in my blood" + "," + " " + 10)
+    let result =  multiply("JavaScript is in my blood", 10);
+    
