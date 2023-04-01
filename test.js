19a323b2bcff7f45b52b83fd26e7c2a19f920fb6
Valerii219 <valera.arl96@gmail.com>
2023-04-01 10:33:54 +0200

    Update test.js

--- test.js
+++ test.js
@@ -36,18 +36,38 @@
 //      console.log(number);
 //    }
 //    return total = total + number;
-   
-//      // Change code above this line
-//    }
 
-function createArrayOfNumbers(min, max) {
-  const numbers = [1, 3];
+// //      // Change code above this line
+// //    }
+
+// function createArrayOfNumbers(min, max) {
+//   const numbers = [1, 3];
+//   // Change code below this line
+//   for (let i = 0; i < numbers.length; i +=1) 
+//   numbers[0] = min;
+//   numbers[1] = max;
+//   numbers =  numbers[0].push(numbers[0] + 1);
+
+//   // Change code above this line
+//   return numbers;
+// }
+
+
+
+function calculateTotalPrice(productName) {
   // Change code below this line
-  for (let i = 0; i < numbers.length; i +=1) 
-  numbers[0] = min;
-  numbers[1] = max;
-  numbers =  numbers[0].push(numbers[0] + 1);
+  const products = [
+    { name: "Radar", price: 1300, quantity: 4 },
+    { name: "Scanner", price: 2700, quantity: 3 },
+    { name: "Droid", price: 400, quantity: 7 },
+    { name: "Grip", price: 1200, quantity: 9 },
+  ];
+  
+
+
+  
+
 
   // Change code above this line
-  return numbers;
 }
+
